7725d8f40f858a220eb10e16b2a81b437c779ea5
Krzysztof Lachowski <s31425@pjwstk.edu.pl>
2026-03-25 13:54:15 +0100

    Dodanie logiki biznesowej

---
+++ RentalApp/Services/PenaltyCalculator.cs
@@ -0,0 +1,14 @@
+﻿using RentalApp.Domain;
+
+namespace RentalApp.Services;
+
+public class PenaltyCalculator
+{
+    public decimal Calculate(Rental rental)
+    {
+        if (!rental.IsLate) return 0;
+
+        int daysLate = (rental.ReturnDate.Value - rental.DueDate).Days;
+        return daysLate * 10;
+    }
+}
---
+++ RentalApp/Services/RentalService.cs
@@ -0,0 +1,37 @@
+﻿using RentalApp.Domain;
+using RentalApp.Repositories;
+
+namespace RentalApp.Services;
+
+public class RentalService
+{
+    private readonly RepozytoriumInterfejs<Rental> _rentals;
+
+    public RentalService(RepozytoriumInterfejs<Rental> rentals)
+    {
+        _rentals = rentals;
+    }
+
+    public void Borrow(User user, Equipment equipment, int days)
+    {
+        if (!equipment.IsAvailable)
+            throw new Exception("Sprzęt niedostępny");
+
+        int active = _rentals.GetAll()
+            .Count(r => r.User.Id == user.Id && !r.IsReturned);
+
+        if (active >= user.MaxBorrowLimit)
+            throw new Exception("Limit przekroczony");
+
+        var rental = new Rental(user, equipment, days);
+
+        equipment.MarkUnavailable();
+        _rentals.Add(rental);
+    }
+
+    public void Return(Rental rental)
+    {
+        rental.Return();
+        rental.Equipment.MarkAvailable();
+    }
+}
