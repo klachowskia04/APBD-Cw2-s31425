a4c76b592b6383784e1ef295dce614190767166d
Krzysztof Lachowski <s31425@pjwstk.edu.pl>
2026-03-25 13:52:47 +0100

    Dodano klase Equipment

---
+++ RentalApp/Domain/Equipment.cs
@@ -0,0 +1,25 @@
+﻿namespace RentalApp.Domain;
+
+public abstract class Equipment
+{
+    public Guid Id { get; private set; }
+    public string Name { get; set; }
+    public bool IsAvailable { get; private set; }
+
+    protected Equipment(string name)
+    {
+        Id = Guid.NewGuid();
+        Name = name;
+        IsAvailable = true;
+    }
+
+    public void MarkUnavailable()
+    {
+        IsAvailable = false;
+    }
+
+    public void MarkAvailable()
+    {
+        IsAvailable = true;
+    }
+}
