6004abf465e8b5c77cf3927cbc4ced0db304d64a
Krzysztof Lachowski <s31425@pjwstk.edu.pl>
2026-03-25 13:54:01 +0100

    Dodanie repo

---
+++ RentalApp/Repositories/RepozytoriumInterfejs.cs
@@ -0,0 +1,7 @@
+﻿namespace RentalApp.Repositories;
+
+public interface RepozytoriumInterfejs<T>
+{
+    void Add(T item);
+    List<T> GetAll();
+}
---
+++ RentalApp/Repositories/RepozytoriumMemory.cs
@@ -0,0 +1,16 @@
+﻿namespace RentalApp.Repositories;
+
+public class InMemoryRepository<T> : RepozytoriumInterfejs<T>
+{
+    private readonly List<T> _items = new();
+
+    public void Add(T item)
+    {
+        _items.Add(item);
+    }
+
+    public List<T> GetAll()
+    {
+        return _items;
+    }
+}
