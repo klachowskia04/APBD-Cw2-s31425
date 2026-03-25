0233ec650e0ec798fb3e4a7fb2112099d757605e
Krzysztof Lachowski <s31425@pjwstk.edu.pl>
2026-03-25 13:53:47 +0100

    Dodano model wypożyczenia

---
+++ RentalApp/Domain/Rental.cs
@@ -0,0 +1,30 @@
+﻿namespace RentalApp.Domain;
+
+public class Rental
+{
+    public Guid Id { get; private set; }
+    public User User { get; }
+    public Equipment Equipment { get; }
+    public DateTime BorrowDate { get; }
+    public DateTime DueDate { get; }
+    public DateTime? ReturnDate { get; private set; }
+
+    public bool IsReturned => ReturnDate.HasValue;
+
+    public Rental(User user, Equipment equipment, int days)
+    {
+        Id = Guid.NewGuid();
+        User = user;
+        Equipment = equipment;
+        BorrowDate = DateTime.Now;
+        DueDate = BorrowDate.AddDays(days);
+    }
+
+    public void Return()
+    {
+        ReturnDate = DateTime.Now;
+    }
+
+    public bool IsLate =>
+        ReturnDate.HasValue && ReturnDate > DueDate;
+}
